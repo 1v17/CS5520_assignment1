387de5f76553f09c6fd79176eb77555eb1404af7
1v17 <166570730+1v17@users.noreply.github.com>
2024-09-22 19:43:22 -0700

    add input box and invalid warning in start screen

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,19 +1,39 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 import React from 'react'
 import Checkbox from 'expo-checkbox';
+import { useState } from 'react';
 
 import GradientBackground from '../components/GradientBackground'
 import colors from '../components/Colors'
 import Card from '../components/Card'
 
 const StartScreen = () => {
+
+  const [text, setText] = React.useState('');
+  const [showWarning, setShowWarning] = React.useState(false);
+  const namePattern = /^[_-a-zA-Z ]+$/;
+
   return (
     <GradientBackground>
       <View style={styles.container}>
         <Card>
 
           <Text style={styles.label} >Name</Text>
+          <TextInput style={styles.input}
+            placeholder="Your name"
+            keyboardType='default'
+            value={text}
+            autoFocus={true}
+            onChangeText={function (changedText) {
+              setText(changedText);
+            }}
+            onBlur={() => {setShowWarning(false)}}
+            onFocus={() => {setShowWarning(true)}}
+           />
+           {!namePattern.test(text) && <Text style={styles.invalid_warning}>
+              Please enter a valid name</Text>}
           <Text style={styles.label} >Email Address</Text>
+          <Text style={styles.label} >Phone number</Text>
           <Checkbox />
 
         </Card>
@@ -34,6 +54,19 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: colors.primary,
     margin: 5,
+  },
+  input: {
+    width: '80%',
+    padding: 10,
+    borderColor: colors.primary,
+    borderBottomWidth: 2,
+    borderRadius: 5,
+    margin: 5,
+    fontSize: 17,
+  },
+  invalid_warning: {
+    color: colors.secondary,
+    fontSize: 10,
   },
 });
 
