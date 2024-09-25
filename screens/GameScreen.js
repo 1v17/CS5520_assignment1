ccc4d2b5be2bd3a11f8179b1ec9bbe64fb46ead1
1v17 <166570730+1v17@users.noreply.github.com>
2024-09-24 17:49:15 -0700

    add a game screen

---
+++ screens/GameScreen.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet, Modal } from 'react-native';
+import React from 'react';
+
+const GameScreen = ({userPhoneNumber}) => {
+
+  const numberPattern = /^(100|[1-9][0-9]?|0)$/;
+
+  return (
+    <View>
+      <Text>GameScreen</Text>
+    </View>
+  )
+}
+
+export default GameScreen
