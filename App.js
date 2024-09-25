d1edb3de6d46e66eae0ef931fe83eeb7dcd093ee
1v17 <166570730+1v17@users.noreply.github.com>
2024-09-24 17:51:51 -0700

    rename the visibility setter and getter of confirm screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 import StartScreen from './screens/StartScreen';
 import ConfirmScreen from './screens/ConfirmScreen';
+import GameScreen from './screens/GameScreen';
 import colors from './components/Colors';
 
 export default function App() {
@@ -11,18 +12,18 @@ export default function App() {
   const [receivedName, setReceivedName] = useState("");
   const [receivedEmail, setReceivedEmail] = useState("");
   const [receivedPhoneNumber, setReceivedPhoneNumber] = useState("");
-  const [modalVisible, setModalVisible] = useState(false);
+  const [confirmScreenVisible, setConfirmScreenVisible] = useState(false);
 
   function handleRegister(name, email, phoneNumber) {
     setReceivedName(name);
     setReceivedEmail(email);
     setReceivedPhoneNumber(phoneNumber);
     // console.log("name:", name,"email:", email,"phone:", phoneNumber);
-    setModalVisible(true);
+    setConfirmScreenVisible(true);
   }
 
   function handleGoBack() {
-    setModalVisible(false);
+    setConfirmScreenVisible(false);
   }
 
   return (
@@ -34,7 +35,7 @@ export default function App() {
       />
 
       <ConfirmScreen 
-        modalVisibile={modalVisible}
+        modalVisibile={confirmScreenVisible}
         userName={receivedName}
         userEmail={receivedEmail}
         userPhoneNumber={receivedPhoneNumber}
