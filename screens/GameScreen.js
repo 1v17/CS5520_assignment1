b978127163e468c86cf6ad84f9288f9e2ff089a7
1v17 <166570730+1v17@users.noreply.github.com>
2024-09-25 17:47:35 -0700

    add the starting screen for game

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, Modal } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, Modal, Alert } from 'react-native';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 
 import GradientBackground from '../components/GradientBackground';
@@ -12,27 +12,83 @@ const GameScreen = ({userPhoneNumber, restartHandler}) => {
 
   const numberPattern = /^(100|[1-9][0-9]?|0)$/;
   const lastDigitOfPhone = parseInt(userPhoneNumber.slice(-1));
-  const LOWER_BOUND = 1;
-  const UPPER_BOUND = 100;
-  const MULTIPLIER_UPPER_BOUND = 50;
+  const lowerBound = 1;
+  const upperBound = 100;
+  const multiplerUpperBound = 50;
+  const maxAttempts = 4;
+  const sixtySeconds = 60;
+  const gameInstructions = 'Guess a number between 1 & 100 that is multiply of ' + lastDigitOfPhone;
 
+  const [target, setTarget] = useState(generateTarget(lastDigitOfPhone));
   const [userGuess, setUserGuess] = useState('');
-
+  const [attemptsLeft, setAttemptsLeft] = useState(maxAttempts);
+  const [timeLeft, setTimeLeft] = useState(sixtySeconds);
+  const [gameOver, setGameOver] = useState(false);
+  const [feedback, setFeedback] = useState('');
+  const [hintUsed, setHintUsed] = useState(false);
+  const [hintVisible, setHintVisible] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
 
   function generateTarget(base) {
     let result;
     do {
-      const multiplier = Math.floor(Math.random() * MULTIPLIER_UPPER_BOUND) + 1;
+      const multiplier = Math.floor(Math.random() * multiplerUpperBound) + 1;
       result = base * multiplier;
-    } while (result < LOWER_BOUND || result > UPPER_BOUND);
+    } while (result < lowerBound || result > upperBound);
     return result;
   }
 
-  const randomMultiple = generateTarget(lastDigitOfPhone);
+  function handleStart() {
+    setGameStarted(true);
+  }
 
   function handleReset() {
     restartHandler();
   }
+
+  function handleNewGame() {
+    setTarget(generateTarget(lastDigitOfPhone));
+    setAttemptsLeft(maxAttempts);
+    setTimeLeft(sixtySeconds);
+    setGameOver(false);
+    setFeedback('');
+    setHintUsed(false);
+    setHintVisible(false);
+    setGameStarted(false);
+    setUserGuess('');
+  }
+
+  function handleGuess() {
+    if (numberPattern.test(userGuess) && parseInt(userGuess) % lastDigitOfPhone === 0) {
+      if (parseInt(userGuess) === target) {
+        setGameOver(true);
+        setFeedback('You guessed correct!\nAttempts used: ' + (maxAttempts - attemptsLeft + 1));
+      } else if (userGuess > target) {
+        setFeedback('You did not guess correct!\nYou should guess lower.');
+        setAttemptsLeft(attemptsLeft - 1);
+      } else {
+        setFeedback('You did not guess correct!\nYou should guess higher.');
+        setAttemptsLeft(attemptsLeft - 1);
+      }
+      if (attemptsLeft === 1) {
+        setGameOver(true);
+        setFeedback('You are out of attempts');
+      }
+    } else {
+      Alert.alert('Invalid number',
+        `Number has to be a multiply of ${lastDigitOfPhone} between 1 and 100`);
+    }
+  }
+
+  useEffect(() => {
+    if (gameStarted && timeLeft > 0) {
+      const timer = setInterval(() => setTimeLeft(timeLeft - 1), 1000);
+      return () => clearInterval(timer);
+    } else if (timeLeft === 0) {
+      setGameOver(true);
+      setFeedback('You are out of time');
+    }
+  }, [timeLeft, gameStarted]);
 
   return (
     <GradientBackground>
@@ -46,11 +102,35 @@ const GameScreen = ({userPhoneNumber, restartHandler}) => {
             disabled={false}
           />
         </View>
-        <View style={styles.container}>
-          <Card>
-            <Text>Game</Text>
-          </Card>
-        </View>
+
+        { // the card for game over
+          gameOver && 
+          <View style={styles.container}>
+            <Card>
+              <Text>The game is over!</Text>
+              <Text>{feedback}</Text>
+            </Card>
+          </View>
+        }
+
+        { // the card for game
+          !gameStarted &&
+          <View style={styles.container}>
+            <Card>
+              <Text>{gameInstructions}</Text>
+              <CustomButton
+                title="Start"
+                pressHandler={handleStart}
+                color={colors.mainButton}
+                disabled={false}
+              />
+            </Card>
+          </View>
+        }
+
+
+
+
       </View>
     </GradientBackground>
   )
